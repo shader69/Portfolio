e442757310e51f30e7eaf64034b560575583f5e8
Kyliann <kyliann.lliteras@laposte.net>
2024-02-06 16:40:43 +0100

    [REFACTOR] Create appInfo file

---
+++ src/data/info.ts
@@ -0,0 +1,10 @@
+export const appInfo = {
+    myName: "Kyliann LLITERAS",
+    jobDescription: "Développeur web back-end",
+    socialMedia: [
+        { label: 'GitHub', href: 'https://github.com/shader69', icon: 'github-logo' },
+        { label: 'LinkedIn', href: 'https://www.linkedin.com/in/kyliann-l-a0ba89142', icon: 'linkedin-logo' },
+        { label: 'Discord', href: 'https://discordapp.com/users/226688624207462420', icon: 'discord-logo' },
+        { label: 'Twitter', href: 'https://twitter.com/shader96', icon: 'twitter-logo' },
+    ],
+};
